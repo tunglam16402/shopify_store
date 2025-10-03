b4f7187f59109507e3e2db59c2fc7c38d4ac0d9b
tunglam16402 <lamnguyen16042002@gmail.com>
2025-10-03 17:01:39 +0700

    test shopify analytic

--- shopify/hooks/use-shopify-analytics.ts
+++ shopify/hooks/use-shopify-analytics.ts
@@ -1,90 +1,92 @@
-// import { usePathname } from 'next/navigation'
-// import {
-//   AnalyticsEventName,
-//   getClientBrowserParameters,
-//   sendShopifyAnalytics,
-//   ShopifyAnalyticsProduct,
-//   ShopifyPageViewPayload,
-//   ShopifySalesChannel,
-//   useShopifyCookies,
-// } from '@shopify/hydrogen-react'
-// import { currency, defaultLanguage } from '../../lib/constants'
+import {
+  AnalyticsEventName,
+  getClientBrowserParameters,
+  sendShopifyAnalytics,
+  ShopifyAnalyticsProduct,
+  ShopifyPageViewPayload,
+  ShopifySalesChannel,
+  useShopifyCookies,
+} from '@shopify/hydrogen-react'
+import { usePathname } from 'next/navigation'
+import { currency, defaultLanguage } from '../../lib/constants'
 
-// const SHOP_ID = process.env.NEXT_PUBLIC_SHOPIFY_SHOP_ID!
+const SHOP_ID = '94567858492'
 
-// type SendPageViewPayload = {
-//   pageType?: string
-//   products?: ShopifyAnalyticsProduct[]
-//   collectionHandle?: string
-//   searchString?: string
-//   totalValue?: number
-//   cartId?: string
-// }
+type SendPageViewPayload = {
+  pageType?: string
+  products?: ShopifyAnalyticsProduct[]
+  collectionHandle?: string
+  searchString?: string
+  totalValue?: number
+  cartId?: string
+}
 
-// type SendAddToCartPayload = {
-//   cartId: string
-//   products?: ShopifyAnalyticsProduct[]
-//   totalValue?: ShopifyPageViewPayload['totalValue']
-// }
+type SendAddToCartPayload = {
+  cartId: string
+  products?: ShopifyAnalyticsProduct[]
+  totalValue?: ShopifyPageViewPayload['totalValue']
+}
 
-// export function useShopifyAnalytics() {
-//   const pathname = usePathname()
-//   // Send page view event
-//   const sendPageView = (
-//     eventName: keyof typeof AnalyticsEventName,
-//     payload?: SendPageViewPayload
-//   ) => {
-//     const eventPayload = {
-//       ...getClientBrowserParameters(),
-//       hasUserConsent: true,
-//       shopifySalesChannel: ShopifySalesChannel.headless,
-//       shopId: `gid://shopify/Shop/${SHOP_ID}`,
-//       currency,
-//       acceptedLanguage: defaultLanguage,
-//       ...payload,
-//     }
+export function useShopifyAnalytics() {
+  const pathname = usePathname()
+  // Send page view event
+  const sendPageView = (
+    eventName: keyof typeof AnalyticsEventName,
+    payload?: SendPageViewPayload
+  ) => {
+    const eventPayload = {
+      ...getClientBrowserParameters(),
+      hasUserConsent: true,
+      shopifySalesChannel: ShopifySalesChannel.hydrogen,
+      shopId: `gid://shopify/Shop/${SHOP_ID}`,
+      currency,
+      acceptedLanguage: defaultLanguage,
+      ...payload,
+    }
 
-//     console.log('>>> Sending analytics event', eventName, eventPayload)
+    return sendShopifyAnalytics({
+      eventName,
+      payload: eventPayload,
+    },'shopif-y-dev-store.myshopify.com')
+  }
 
-//     return sendShopifyAnalytics({
-//       eventName,
-//       payload: eventPayload,
-//     })
-//   }
+  // Send add to cart event
+  const sendAddToCart = ({
+    cartId,
+    totalValue,
+    products,
+  }: SendAddToCartPayload) => {
+    const browserParams = getClientBrowserParameters() // uniqueToken, visitToken, navigationType, navigationApi
+    const payload = {
+      cartId,
+      products,
+      totalValue,
+      shopId: `gid://shopify/Shop/${SHOP_ID}`,
+      shopifySalesChannel: ShopifySalesChannel.hydrogen,
+      currency,
+      hasUserConsent: true,
+      ...browserParams,
+    }
 
-//   // Send add to cart event
-//   const sendAddToCart = ({
-//     cartId,
-//     totalValue,
-//     products,
-//   }: SendAddToCartPayload) => {
-//     const browserParams = getClientBrowserParameters() // uniqueToken, visitToken, navigationType, navigationApi
-//     const payload = {
-//       cartId,
-//       products,
-//       totalValue,
-//       shopId: `gid://shopify/Shop/${SHOP_ID}`,
-//       shopifySalesChannel: ShopifySalesChannel.headless,
-//       currency,
-//       hasUserConsent: true,
-//       ...browserParams,
-//     }
+    return sendShopifyAnalytics(
+      {
+        eventName: AnalyticsEventName.ADD_TO_CART,
+        payload,
+      },
+      'shopif-y-dev-store.myshopify.com'
+    )
+  }
 
-//     return sendShopifyAnalytics({
-//       eventName: AnalyticsEventName.ADD_TO_CART,
-//       payload,
-//     })
-//   }
+  // Set up cookies for Shopify analytics & enable user consent
+  useShopifyCookies({
+    hasUserConsent: true,
+    domain: 'shopif-y-dev-store.myshopify.com',
+    checkoutDomain: 'shopif-y-dev-store.myshopify.com',
+  })
 
-//   // Set up cookies for Shopify analytics & enable user consent
-//   useShopifyCookies({
-//     hasUserConsent: true,
-//   })
-
-//   return {
-//     sendPageView,
-//     sendAddToCart,
-//     pathname,
-//   }
-// }
-
+  return {
+    sendPageView,
+    sendAddToCart,
+    pathname,
+  }
+}
